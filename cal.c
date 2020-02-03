583ce4f560b2aaf443dc73e125df96a08ca7baa3
JMJ Riyana <jeemariyana@gmail.com>
2020-02-03 13:42:24 +0530

    display

---
+++ cal.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+ int main{
+     printf("CALCULATOR");
+     printf("====================");
+     printf("press 1 to addition");
+     printf("press 2 to subtraction");
+     printf("press 3 to division");
+     printf("press 4 to multipication");
+     printf("====================");
+     return 0;
+ }
