93135073beb794c083084920b7e7128af2035eed
JMJ Riyana <jeemariyana@gmail.com>
2020-02-03 14:14:04 +0530

    addition

--- cal.c
+++ cal.c
@@ -1,11 +1,28 @@
 #include<stdio.h>
- int main{
-     printf("CALCULATOR");
-     printf("====================");
-     printf("press 1 to addition");
-     printf("press 2 to subtraction");
-     printf("press 3 to division");
-     printf("press 4 to multipication");
-     printf("====================");
+ int main(){
+     printf("\tCALCULATOR\n");
+     printf("========================\n");
+     printf("press 1 to addition\n");
+     printf("press 2 to subtraction\n");
+     printf("press 3 to division\n");
+     printf("press 4 to multipication\n");
+     printf("========================\n\n");
+     
+     int n;
+     printf("enter number:");
+     scanf("%d",&n);
+     switch (n)
+     {
+         case 1:
+         printf("Addition\n");
+         float a,b;
+         printf("enter first number:");
+         scanf("%f",&a);
+         printf("enter second number:");
+         scanf("%f",&b);
+         float sum=(a+b)/2;
+         printf("\nSum of %f and %f is %f.",a,b,sum);
+         
+     }
      return 0;
  }
