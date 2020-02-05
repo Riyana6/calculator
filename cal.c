f21f1f452b5411d532088ddaa4e2bc65e9ebded4
JMJ Riyana <jeemariyana@gmail.com>
2020-02-05 10:49:53 +0530

    division

--- cal.c
+++ cal.c
@@ -35,6 +35,30 @@
          printf("\nDifference between %f and %f is %f.",a1,b1,sub);
          break;
 
+         case 3:
+         printf("Division\n");
+         float a2,b2;
+         printf("enter first number:");
+         scanf("%f",&a2);
+         printf("enter second number:");
+         scanf("%f",&b2);
+         float div=(a2/b2);
+         printf("\nDivision of %f and %f is %f.",a2,b2,div);
+         break;
+         
+         case 3:
+         printf("Subtraction\n");
+         float a1,b1;
+         printf("enter first number:");
+         scanf("%f",&a1);
+         printf("enter second number:");
+         scanf("%f",&b1);
+         float sub=(a1-b1);
+         printf("\nDifference between %f and %f is %f.",a1,b1,sub);
+         break;
+
+
+
         
      }
      return 0;
